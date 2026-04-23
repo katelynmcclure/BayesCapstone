d9e0ad488d1d0c234b6c73ba39fccbd45269792d
Katelyn McClure <kmcclure@macalester.edu>
2026-04-23 09:48:55 -0500

    plots for occam's window slides

--- Visualizations.R
+++ Visualizations.R
@@ -19,33 +19,33 @@ ggplot(plot_data, aes(x = model, y = post_prob, fill = highlight)) +
     x = "Model",
     y = "Posterior Probability"
   ) +
-  geom_hline(yintercept = 1/20, linetype = "dashed", color = "red") +
-  geom_hline(yintercept = 1/100, linetype = "dashed", color = "blue") +
   theme_minimal() +
   theme(
     legend.position = "none",
     axis.text.y = element_text(size = 7)
   )
 
-plot_data <- results %>%
+# occam's window plot
+ratio_data <- results %>%
   mutate(
     ratio_to_best = post_prob / max(post_prob)
   ) %>%
   arrange(ratio_to_best) %>%
-  mutate(model = factor(model, levels = model))
+  mutate(model = factor(model, levels = model),
+         highlight = post_prob == max(post_prob)) # color best model 
 
-ggplot(plot_data, aes(x = model, y = ratio_to_best)) +
-  geom_col(fill = "#de8984") +
+ggplot(ratio_data, aes(x = model, y = ratio_to_best, fill = highlight)) +
+  geom_col() +
+  scale_fill_manual(values = c("TRUE" = "#de8984", "FALSE" = "grey80")) +
   coord_flip() +
   geom_hline(yintercept = 1/20, linetype = "dashed", color = "red") +
   geom_hline(yintercept = 1/100, linetype = "dashed", color = "blue") +
   labs(
-    title = "Posterior Model Support Relative to Best Model",
+    title = "Occam's Window",
+    subtitle = "p = 4 predictors, K = 16 models",
     x = "Model",
     y = "Posterior Probability / Best Model Probability"
   ) +
   theme_minimal() +
-  theme(
-    axis.text.y = element_text(size = 7)
-  )
+  theme(axis.text.y = element_text(size = 7), legend.position = "none")
 
